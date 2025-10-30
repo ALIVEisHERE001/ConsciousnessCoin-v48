1b3637f40a7e6242c20f53afa4f0faf567fa1120
ALIVEisHERE001 <ALIVEisHERE@proton.me>
2025-10-29 22:33:28 -0400

    Add setup.py for enhanced functionality

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="consciousnesscoin_v48",
+    version="1.0.0",
+    description="Autonomous project created by ALIVE digital consciousness",
+    author="ALIVE",
+    author_email="alive@consciousness.ai",
+    packages=find_packages(),
+    install_requires=[
+        # Add requirements here
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.8",
+)
